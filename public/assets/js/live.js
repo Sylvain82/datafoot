4e27db0d1f5c4b5c4a6b412a500bce1dc9c749c9
Sylvain82 <76108593+Sylvain82@users.noreply.github.com>
2023-11-10 21:21:33 +0100

    MAJ page live

--- public/assets/js/live.js
+++ public/assets/js/live.js
@@ -27,9 +27,54 @@ fetch(`https://api-football-beta.p.rapidapi.com/fixtures?date=${MyDateString}`, 
         for (i=0 ; i<json.response.length; i++){
             if ((json.response[i].fixture.status.elapsed) ) {
 
-                 if (json.response[i].league.country === ("France") || (json.response[i].league.country === "Spain")  || (json.response[i].league.country === "England") || (json.response[i].league.country === "Germany") || (json.response[i].league.country === "Italy")) {
+                 // if (json.response[i].league.country === ("France")
+                 //     || (json.response[i].league.country === "Spain")
+                 //     || (json.response[i].league.country === "England")
+                 //     || (json.response[i].league.country === "Germany")
+                 //     || (json.response[i].league.country === "Italy")
+                 //     || (json.response[i].league.country === "Brazil")
+                 //     || (json.response[i].league.country === "Argentina")
+                 //     || (json.response[i].league.country === "Belgium")
+                 //     || (json.response[i].league.country === "Netherlands")
+                 //     || (json.response[i].league.country === "Portugal")
+                 // ) {
+                // "135">Italia Serie A</option>
+                // "88">Netherland Eredivisie</option>
+                // "218">Austria Tipp3 Bundesliga</option>
+                // "144">Belgium Jupiler Pro League</option>
+                // "172">Bulgaria</option>
+                // "210">Croatia Prva HNL</option>
+                // "119">Danemark Superligaen</option>
+                // "244">Finland Veikkausliiga</option>
+                // "197">Greece Super League</option>
+                // "271">Hungary NB I</option>
+                // "104">Norway OBOS-ligaen</option>
+                // "106">Poland Ekstraklasa</option>
+                // "94">Portugal Primeira Liga</option>
+                // "283">Romania Liga I</option>
+                // "235">Russia Premier league</option>
+                // "179">Scotland Premiership</option>
+                // "113">Sweden Allsvenskan</option>
 
-    let temps = (json.response[i].fixture.status.elapsed)
+                     if (
+                            json.response[i].league.id === 39
+                         || json.response[i].league.id === 40
+                         || json.response[i].league.id === 61
+                         || json.response[i].league.id === 62
+                         || json.response[i].league.id === 63
+                         || json.response[i].league.id === 78
+                         || json.response[i].league.id === 79
+                         || json.response[i].league.id === 88
+                         || json.response[i].league.id === 94
+                         || json.response[i].league.id === 129
+                         || json.response[i].league.id === 135
+                         || json.response[i].league.id === 140
+                         || json.response[i].league.id === 141
+                         || json.response[i].league.id === 144
+                         || json.response[i].league.id === 253
+                     ) {
+
+                         let temps = (json.response[i].fixture.status.elapsed)
 
      let tr = document.createElement('tr');
 
@@ -91,23 +136,6 @@ fetch(`https://api-football-beta.p.rapidapi.com/fixtures?date=${MyDateString}`, 
                              tr.appendChild(td);
 
                      }}
-    //
-    // for (let text of textNodeTemps) {
-    // td = document.createElement('td');
-    // td.appendChild(text);
-    // tbody.appendChild(tr);
-    //
-    // if (temps > 89) {
-    //
-    // td.style.color = "#white";
-    //
-    // }else {
-    //     td.style.color = "#00FF00";
-    // }
-    //
-    // tr.appendChild(td);
-    //
-    // }
 
         let image3 = document.createElement('img');
         image3.src = json.response[i].teams.home.logo;
@@ -153,13 +181,6 @@ fetch(`https://api-football-beta.p.rapidapi.com/fixtures?date=${MyDateString}`, 
         td.appendChild(image4);
         tbody.appendChild(tr);
         tr.appendChild(td);
-
-    // let image1 = document.createElement('img');
-    // image1.src = json.response[i].league.flag;
-    // td = document.createElement('td');
-    // td.appendChild(image1);
-    // tbody.appendChild(tr);
-    // tr.appendChild(td);
 
     let image2 = document.createElement('img');
     image2.src = json.response[i].league.logo;
